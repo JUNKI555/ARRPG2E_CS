ca709fa9f166c4108551d0ac4f6ae2a0826e5006
JUNKI555 <junki555@gmail.com>
2020-09-28 18:26:50 +0900

    :pencil2: fix ids

--- src/components/ClassInfoLv1.svelte
+++ src/components/ClassInfoLv1.svelte
@@ -9,16 +9,16 @@
       <tr>
         <td>
           <div class="nes-field is-inline">
-            <label for="input_main_class">メインクラス</label>
-            <input type="text" id="input_main_class" class="nes-input is-dark" size="40">
+            <label for="input_lv1_main_class">メインクラス</label>
+            <input type="text" id="input_lv1_main_class" class="nes-input is-dark" size="40">
           </div>
         </td>
       </tr>
       <tr>
         <td>
           <div class="nes-field is-inline">
-            <label for="input_support_class">サポートクラス</label>
-            <input type="text" id="input_support_class" class="nes-input is-dark">
+            <label for="input_lv1_support_class">サポートクラス</label>
+            <input type="text" id="input_lv1_support_class" class="nes-input is-dark">
           </div>
         </td>
       </tr>
